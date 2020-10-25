1150c8fdf6e456c110cbd2719d40434223c66507
kai-blt <blaitong@gmail.com>
2020-10-25 10:23:39 -0700

    setup basic component architecture

---
+++ src/components/Todo.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ToDo(props) {
+    return(
+        <div>This is a list item </div>
+    )
+}
---
+++ src/components/TodoForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ToDoForm(props) {
+    return(
+        <form>
+            <input></input>
+            <button>Add</button>
+            <button>Clear</button>
+        </form>
+    )
+}
